b1cf1b9794a6f4285ff1c5d504b7b1ad05f883ed
sandeep.sutar <sutarsandeep155@gmail.com>
2018-12-22 22:06:37 -0800

    Added function roman_to_integer()

---
+++ Scripts/Python/Common/Transform/transform_string.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+def roman_to_integer(in_str):
+    '''
+    Purpose : Convert input roman number (string) to integer equivalent.
+    Input   : str
+    Return  : int
+    Example : f('XXXIX')=39; f('XXX'):30; f('CVII')=107; f('XCIX')=99
+    '''
+    substract = 0
+    out_num = 0
+    dictionary = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    for i in range(0, len(in_str)):
+        if i == len(in_str)-1:
+            out_num += dictionary[in_str[i]] - substract
+            substract = 0
+        elif dictionary[in_str[i]] < dictionary[in_str[i+1]]:
+            substract = dictionary[in_str[i]]
+        elif dictionary[in_str[i]] >= dictionary [in_str[i]]:
+            out_num += dictionary[in_str[i]] - substract
+            substract = 0
+        i+=1
+    return out_num
+
+if __name__ == "__main__":
+    print ("roman_to_integer({}) = {}".format(sys.argv[1], roman_to_integer(sys.argv[1])))
