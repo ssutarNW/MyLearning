2cdf908fb037dac683c4f10f651a535eee780417
Sandeep Sutar <42618884+ssutarNW@users.noreply.github.com>
2019-01-27 14:38:42 -0800

    Add transformation scripts (#1)
    Added transformation scripts

---
+++ Scripts/Python/Common/Transform/transform_file.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import json
+
+def csv_to_json(in_file, out_file):
+    """
+    Purpose : Write to new JSON file with the content from CSV file.
+              Assume that your function returns 0 when the reversed integer overflows.
+    Input   : in_file  (str) - Input CSV file path
+            : out_file (str) - Output JSON file path
+    Example : csv_to_json('csv.txt', 'json.txt')
+    """
+    with open(out_file, 'w') as json_file:
+        with open(in_file, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                json.dump(row, json_file)
